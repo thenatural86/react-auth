0bb313e691351153f36278cf1aa95750d75568f9
thenatural86 <philip.smalls86@gmail.com>
2021-05-24 13:25:38 -0400

    signing up a user

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,25 +1,81 @@
-import { useState } from 'react';
+import { useState } from 'react'
 
-import classes from './AuthForm.module.css';
+import classes from './AuthForm.module.css'
 
 const AuthForm = () => {
-  const [isLogin, setIsLogin] = useState(true);
+  const [isLogin, setIsLogin] = useState(true)
+  const [emailInput, setEmailInput] = useState('')
+  const [passwordInput, setPasswordInput] = useState('')
 
   const switchAuthModeHandler = () => {
-    setIsLogin((prevState) => !prevState);
-  };
+    setIsLogin((prevState) => !prevState)
+  }
+
+  const submitFormHandler = (e) => {
+    e.preventDefault()
+
+    const enteredEmail = emailInput
+    const enteredPassword = passwordInput
+
+    if (isLogin) {
+    } else {
+      fetch(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBR1IcrFFLsrP3RqI2WqCRnwFkRdsQzSiY',
+        {
+          method: 'POST',
+          body: JSON.stringify({
+            email: enteredEmail,
+            password: enteredPassword,
+            returnSecureToken: true,
+          }),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      ).then((res) => {
+        if (res.ok) {
+          // ...
+        } else {
+          return res.json().then((data) => {
+            // show an error modal
+            console.log(data)
+          })
+        }
+      })
+    }
+  }
+
+  const emailChangeHandler = (e) => {
+    setEmailInput(e.target.value)
+  }
+
+  const passwordChangeHandler = (e) => {
+    setPasswordInput(e.target.value)
+  }
 
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      <form onSubmit={submitFormHandler}>
         <div className={classes.control}>
           <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <input
+            type='email'
+            id='email'
+            required
+            value={emailInput}
+            onChange={emailChangeHandler}
+          />
         </div>
         <div className={classes.control}>
           <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <input
+            type='password'
+            id='password'
+            required
+            value={passwordInput}
+            onChange={passwordChangeHandler}
+          />
         </div>
         <div className={classes.actions}>
           <button>{isLogin ? 'Login' : 'Create Account'}</button>
@@ -33,7 +89,7 @@ const AuthForm = () => {
         </div>
       </form>
     </section>
-  );
-};
+  )
+}
 
-export default AuthForm;
+export default AuthForm
